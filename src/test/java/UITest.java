e5d69f0cbfece6bd20b8134414fd445c4c3a3d00
Mirt12 <tanyabalashevich@gmail.com>
2023-07-22 17:43:43 +0200

    add wait and alert methods

--- src/test/java/UITest.java
+++ src/test/java/UITest.java
@@ -1,24 +1,21 @@
 
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class UITest {
     @Test
-    public void testAlert(){
+    public void alertTest() {
         PageObjectUI po = new PageObjectUI();
         po.getUrl().clickSubmitBtn();
-        WebDriverWait wait = new WebDriverWait(DriverSingleton.getDriver(), Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(DriverSingleton.getDriver(), Duration.ofSeconds(7));
         Alert alert = wait.until(ExpectedConditions.alertIsPresent());
-        Assertions.assertEquals("Заполните форму",  alert.getText());
+        Assertions.assertEquals("Заполните форму", alert.getText());
         alert.accept();
         DriverSingleton.quit();
     }
