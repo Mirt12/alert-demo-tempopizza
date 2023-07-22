8f867fb85d3230931ca44982bf506361d3758a16
Mirt12 <tanyabalashevich@gmail.com>
2023-07-22 14:06:31 +0200

    add wait and alert methods

--- src/test/java/UITest.java
+++ src/test/java/UITest.java
@@ -1,15 +1,25 @@
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
 
 public class UITest {
-//    @Test
-//    public void testAlert(){
-//        PageObject2pizzaTempo po = new PageObject2pizzaTempo();
-//        WebDriverWait wait = new WebDriverWait(DriverSingleton.getDriver(), Duration.ofSeconds(20));
-//        po.getUrl().clickBtnSubmit();
-//        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
-//        String text = alert.getText();
-//        alert.accept();
-//        Assertions.assertEquals("Заполните форму", text);
-//        //Assertions.assertEquals("Заполните форму", po.getAlertTest());
-//    }
+    @Test
+    public void testAlert(){
+        PageObjectUI po = new PageObjectUI();
+        po.getUrl().clickSubmitBtn();
+        WebDriverWait wait = new WebDriverWait(DriverSingleton.getDriver(), Duration.ofSeconds(10));
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+        Assertions.assertEquals("Заполните форму",  alert.getText());
+        alert.accept();
+        DriverSingleton.quit();
+    }
 }
